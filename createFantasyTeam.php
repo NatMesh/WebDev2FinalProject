14dfee15cded62f0f63b671cb5b5f690e81d13c2
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2019-11-29 10:24:55 -0600

    An error message appears if a user has 11 or more players already on there team

--- createFantasyTeam.php
+++ createFantasyTeam.php
@@ -1,7 +1,31 @@
 <!-- Displays a create team page where users who are authenticated can create a team with a given name and then add players from the players table to their team that are available. -->
 <?php 
-	include 'connect2.php';
+	include 'header.php';
+	$teamExists = false;
 
+	if(!(isset($_SESSION['firstName']))){
+		header('Location: login.php');
+	}
+	else{
+		$query = "SELECT * FROM userfantasyteam WHERE userId = :userId";
+
+
+		$statement = $db->prepare($query);
+
+		$statement -> bindvalue(':userId', $_SESSION['userID']);
+
+		$statement->execute();
+
+		$team = $statement->fetch();
+
+		$count = $statement->rowCount();
+
+		if($count == 1){
+			$teamExists = true;
+		}
+	}	
+
+		
 	
 ?>
 
@@ -11,11 +35,22 @@
 	<title>Create Team</title>
 </head>
 <body>
-	<h1>hello</h1>
+	<?php if($teamExists): ?>
+		<p>You can not have more then one team.</p>
+	<?php else: ?>
+		<form action="crudFantasyTeamScript.php" method="POST">
+			<fieldset>
+				<legend>Create Fantasy Team</legend>
+				<p>
+					<label>Fantasy Team Name</label>
+					<input type="text" name="fantasyTeamName" id="fantasyTeamName">
+				</p>
 
-	<form action="crudFantasyTeamScript.php" method="POST">
-		<label>Team Name</label>
-		<input type="text" name="FantasyTeamName" id="FantasyTeamName">
-	</form>
+				<p>
+					<input type="submit" name="CreateTeam" value="Create Team">
+				</p>
+			</fieldset>
+		</form>
+	<?php endif ?>
 </body>
 </html>
